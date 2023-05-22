17f1f9d80260a598202bdf3204a0522a22befbd7
Sparrrgh <ma.bellia.98@gmail.com>
2023-05-22 20:23:20 +0200

    Bugfix
    Fixed bug where if you didn't set the depth, it wouldn't register the setting and thus wouldn't run

--- __init__.py
+++ __init__.py
@@ -7,12 +7,15 @@ from binaryninja.log import (log_error, log_debug, log_alert, log_warn)
 from binaryninja.settings import Settings
 from binaryninja.interaction import get_directory_name_input
 import binaryninjaui
-from binaryninjaui import (getMonospaceFont, UIAction, UIActionHandler, Menu, UIContext)
+from binaryninjaui import (getMonospaceFont, UIAction,
+                           UIActionHandler, Menu, UIContext)
 from PySide6.QtWidgets import (QLineEdit, QPushButton, QApplication, QWidget,
-     QVBoxLayout, QHBoxLayout, QDialog, QFileSystemModel, QTreeView, QLabel, QSplitter,
-     QInputDialog, QMessageBox, QHeaderView, QKeySequenceEdit, QCheckBox, QMenu, QTextEdit)
-from PySide6.QtCore import (QDir, Qt, QFileInfo, QItemSelectionModel, QSettings, QUrl)
-from PySide6.QtGui import (QFontMetrics, QDesktopServices, QKeySequence, QIcon, QColor, QAction)
+                               QVBoxLayout, QHBoxLayout, QDialog, QFileSystemModel, QTreeView, QLabel, QSplitter,
+                               QInputDialog, QMessageBox, QHeaderView, QKeySequenceEdit, QCheckBox, QMenu, QTextEdit)
+from PySide6.QtCore import (
+    QDir, Qt, QFileInfo, QItemSelectionModel, QSettings, QUrl)
+from PySide6.QtGui import (
+    QFontMetrics, QDesktopServices, QKeySequence, QIcon, QColor, QAction)
 
 
 # https://github.com/Vector35/snippets/blob/a10096727be5bb8d17c88fab33ed43ff12a736e4/__init__.py#L190
@@ -20,25 +23,14 @@ from PySide6.QtGui import (QFontMetrics, QDesktopServices, QKeySequence, QIcon, 
 class RopSettings(QDialog):
     def __init__(self, context, parent=None):
         super(RopSettings, self).__init__(parent)
-        self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
+        self.setWindowFlags(self.windowFlags() & ~
+                            Qt.WindowContextHelpButtonHint)
         self.title = QLabel(self.tr("Settings for mips_rop"))
         self.saveButton = QPushButton(self.tr("&Save"))
         self.closeButton = QPushButton(self.tr("Close"))
         self.setWindowTitle(self.title.text())
         self.columns = 2
         self.context = context
-
-        Settings().register_group("ropsettings", "ROP settings")
-
-        Settings().register_setting("ropsettings.depth", """
-            {
-                "title" : "Depth of gadget search",
-                "type" : "number",
-                "default" : 4,
-                "description" : "Maximum depth of the gadgets that will be searched",
-                "ignore" : ["SettingsProjectScope", "SettingsResourceScope"]
-            }
-            """)
 
         depth = Settings().get_integer("ropsettings.depth")
         print(depth)
@@ -69,25 +61,28 @@ class RopSettings(QDialog):
         # Set dialog layout
         self.setLayout(vlayout)
 
-         # Add signals
+        # Add signals
         self.saveButton.clicked.connect(self.save)
         self.closeButton.clicked.connect(self.close)
-        
+
         # self.resize(1000, 640)
         self.showNormal()   # fixes bug that maximized windows are stuck
         self.settings = QSettings("", "mips_rop")
-        
+
     def save(self):
         Settings().set_integer("ropsettings.depth", int(self.depth.text()))
         self.close()
+
 
 def openSettings(context):
     settings = RopSettings(context, parent=context.widget)
     settings.open()
 
+
 def find_rop_gadgets(bv):
     rop_search = ROPSearch(bv)
     rop_search.start()
+
 
 if __name__ == "__main__":
     print("from main")
@@ -97,8 +92,21 @@ else:
         "finds rop gadgets in current binary",
         find_rop_gadgets
     )
-	
+
+    Settings().register_group("ropsettings", "ROP settings")
+    Settings().register_setting("ropsettings.depth", """
+        {
+            "title" : "Depth of gadget search",
+            "type" : "number",
+            "default" : 4,
+            "description" : "Maximum depth of the gadgets that will be searched",
+            "ignore" : ["SettingsProjectScope", "SettingsResourceScope"]
+        }
+        """)
+
     UIAction.registerAction("mips_rop\\Settings")
-    UIActionHandler.globalActions().bindAction("mips_rop\\Settings", UIAction(openSettings))
-    Menu.mainMenu("Plugins").addAction("mips_rop\\Find ROP Gadgets", "mips_rop")
+    UIActionHandler.globalActions().bindAction(
+        "mips_rop\\Settings", UIAction(openSettings))
+    Menu.mainMenu("Plugins").addAction(
+        "mips_rop\\Find ROP Gadgets", "mips_rop")
     Menu.mainMenu("Plugins").addAction("mips_rop\\Settings", "mips_rop")
