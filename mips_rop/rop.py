d19aebb99d15de801c230392b3e25ce6702766b5
Sparrrgh <ma.bellia.98@gmail.com>
2023-05-22 20:24:27 +0200

    Added support for $ra gadgets and more ways to set $t9

--- mips_rop/rop.py
+++ mips_rop/rop.py
@@ -9,7 +9,9 @@ import re
 INSTR_SIZE = 4
 # This is a JALR in t9
 # [ŦODO] Add jumps to other registers even if they are rare
-_RET_INSTRS = {"jalr t9": [b"\x03\x20\xf8\x09"], "jr t9" : [b"\x03\x20\x00\x08"]}
+_JUMP_INSTRS = {"jalr $t9": [b"\x03\x20\xf8\x09"], "jr $t9": [
+    b"\x03\x20\x00\x08"], "jr $ra": [b"\x03\xe0\x00\x08"]}
+
 
 class ROPSearch(BackgroundTaskThread):
     """
@@ -23,7 +25,7 @@ class ROPSearch(BackgroundTaskThread):
         self.progress = "[+] mips_rop: searching for rop gadgets"
         self.endianness = self.bv.perform_get_default_endianness()
         self.arch = self.bv.arch
-        self.ret_instrs = _RET_INSTRS
+        self.jump_instrs = _JUMP_INSTRS
 
     def run(self):
         """
@@ -44,86 +46,96 @@ class ROPSearch(BackgroundTaskThread):
             )
         self.progress = ""
 
-    def _disas_all_instrs(self, start_addr, ret_addr: int) -> list[str]:
+    def _disas_all_instrs(self, start_addr, jump_addr: int) -> list[str]:
         """
         disassemble all instructions in chunk
         """
         instructions: list[str] = []
         curr_addr = start_addr
-        while curr_addr < ret_addr:
+        while curr_addr < jump_addr:
             instr = self.bv.get_disassembly(curr_addr)
             if instr == "":  # bad addr
                 return None
-            if instr in _RET_INSTRS.keys():  # exclude 2 rets
-                return None
             instructions.append(instr)
             curr_addr += self.bv.get_instruction_length(curr_addr)
-        # ret opcode was included in last instruction calculation
-        if curr_addr != ret_addr:
+        # jump opcode was included in last instruction calculation
+        if curr_addr != jump_addr:
             return None
 
         return instructions
 
-    def _calculate_gadget_from_ret(self, gadgets: dict[int, (str, str)], ret_addr: int):
+    def _calculate_gadget_from_jump(self, gadgets: dict[int, (str, str)], jump_addr: int):
         """
-        decrement index from ret ins and calculate gadgets
+        decrement index from jump ins and calculate gadgets
         """
-        ret_addr += 4 
+        jump_instr = self.bv.get_disassembly(jump_addr)
+        jump_addr += 4
+        # [TODO] and $.., $.., $.. is also a possible way load values
+        # addiu comes from https://write.lain.faith/~/Haskal/mips-rop/
+        t9_ctrl = [r"move +\$t9, ", r"addiu +\$t9, ",
+                   r"or +\$t9, \$.., \$zero"]
+        ra_ctrl = [r"lw +\$ra, "]
         for i in range(0, Settings().get_integer("ropsettings.depth")*INSTR_SIZE + 1):
-            #  MIPS has jump delay slots, this means that the instruction 
+            #  MIPS has jump delay slots, this means that the instruction
             # immediatly after the jump is executed with the jump
-            instructions = self._disas_all_instrs(ret_addr - i, ret_addr + 4)
+            instructions = self._disas_all_instrs(jump_addr - i, jump_addr + 4)
             if instructions is None:
                 continue
             gadget_str = ""
             for instr in instructions:
                 gadget_str += f"{instr} ; "
-                
 
             # https://github.com/tacnetsol/ida/blob/master/plugins/mipsrop/mipsrop.py#LL373C58-L373C58
-            move_str = "move    $t9, "
-            add_str = "addiu   $t9, "
             control_reg = ""
-            t9_control_move = gadget_str.find(move_str)
-            if t9_control_move != -1:
-                t9_control_move += len(move_str)
-                control_reg = gadget_str[t9_control_move:gadget_str.find(" ;", t9_control_move)]
+            # [TODO] Search in reverse, in case the control passes through more moves
+            if "$t9" in jump_instr:
+                ctrl_arr = t9_ctrl
+            elif "$ra":
+                ctrl_arr = ra_ctrl
+            else:
+                print(f"Weird jump {gadget_str}")
 
-            t9_control_add = gadget_str.find(add_str)
-            if t9_control_add != -1:
-                t9_control_add += len(add_str)
-                control_reg = gadget_str[t9_control_add:gadget_str.find(" ;", t9_control_add)]
-                if control_reg.__contains__("$t9"):
-                    control_reg = ""
+            for ctrl in ctrl_arr:
+                ctrl_idx = re.search(ctrl, gadget_str)
+                if ctrl_idx:
+                    ctrl_idx = ctrl_idx.end()
+                    control_reg = gadget_str[ctrl_idx:gadget_str.find(
+                        " ;", ctrl_idx)]
+                    break
+
+            if "$t9" in control_reg or "$ra" in control_reg:
+                control_reg = ""
+
             if control_reg != "":
-                gadget_rva = ret_addr - i - self.bv.start
+                gadget_rva = jump_addr - i - self.bv.start
                 gadgets[gadget_rva] = (f"{gadget_str}", control_reg)
         return gadgets
 
-
     def _find_gadgets_in_data(
         self, insts: tuple[list[str], int]
-    ) -> dict[int, (str,str)]:
+    ) -> dict[int, (str, str)]:
         """
-        find ret instructions and spawn a thread to calculate gadgets
+        find jump instructions and spawn a thread to calculate gadgets
         """
         gadgets: dict[int, (str, str)] = dict()
-        for _, bytecodes in _RET_INSTRS.items():
+        for _, bytecodes in _JUMP_INSTRS.items():
             for bytecode in bytecodes:
                 next_start = insts[0][1]
-                next_ret_addr = 0
+                next_jump_addr = 0
                 while next_start < insts[-1][1]:
-                    next_ret_addr = self.bv.find_next_data(next_start, bytecode)
-                    if next_ret_addr is None:
+                    next_jump_addr = self.bv.find_next_data(
+                        next_start, bytecode)
+                    if next_jump_addr is None:
                         break
 
                     # TODO thread this?
-                    gadgets = self._calculate_gadget_from_ret(gadgets, next_ret_addr)
-                    next_start = next_ret_addr + len(bytecode)
+                    gadgets = self._calculate_gadget_from_jump(
+                        gadgets, next_jump_addr)
+                    next_start = next_jump_addr + len(bytecode)
                     # [y[1] for y in ins].index(4199412)
 
         return gadgets
-    
+
     def _generate_output(self, gadgets: dict[int, (str, str)], title: str):
         """
         display rop gadgets
@@ -143,10 +155,14 @@ class ROPSearch(BackgroundTaskThread):
             if gadget not in found:
                 gadget_str = " ".join(gadget[0].split())
                 addr = addr + self.bv.start  # make sure addrs are correct
-                
-                f_gadget_str = (
-                    f"[0x{addr:016x}](binaryninja://?expr={addr:08x})  \tControl reg: <span style='color: green;'>{gadget[1]}</span>\t{gadget_str}\n\n"
-                )
+                if len(gadget[1]) < 4:
+                    f_gadget_str = (
+                        f"[0x{addr:016x}](binaryninja://?expr={addr:08x})  \tControl reg: <span style='color: green;'>{gadget[1]}</span>\t\t\t{gadget_str}\n\n"
+                    )
+                else:
+                    f_gadget_str = (
+                        f"[0x{addr:016x}](binaryninja://?expr={addr:08x})  \tControl reg: <span style='color: green;'>{gadget[1]}</span>\t\t{gadget_str}\n\n"
+                    )
                 all_gadgets += f_gadget_str
 
                 # stackfinder
@@ -166,7 +182,6 @@ class ROPSearch(BackgroundTaskThread):
                     double_jump_gadgets += f_gadget_str
                 # tails?
                 found.append(gadget)
-
 
         markdown += f"[+] found {len(found)} gadgets\n***\n"
 
